470d68e105b5da4a94ab657776bb4e62189b4f1c
MattiaBologna <mattiabologna01@gmail.com>
2024-03-27 15:54:42 +0100

    Add: todo text display

--- js/app.js
+++ js/app.js
@@ -3,7 +3,27 @@
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        todos: [
+            {
+                text: 'Fare la spesa',
+                done: false
+            },
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Allenamento',
+                done: true
+            },
+            {
+                text: 'Fare la todo list',
+                done: true
+            },
+        ]
       }
+    },
+    methods: {
+
     }
   }).mount('#app')
